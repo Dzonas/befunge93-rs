c7f6c0969694018eaa7de04edd0e10f7050e5b97
Jan Cybulski <13089097+Dzonas@users.noreply.github.com>
2024-09-16 19:37:50 +0200

    Add web target

--- src/bin/gui.rs
+++ src/bin/gui.rs
@@ -1,3 +1,4 @@
+#[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result {
     env_logger::init();
 
@@ -13,6 +14,40 @@ fn main() -> eframe::Result {
         native_options,
         Box::new(|cc| Ok(Box::new(Befunge93App::new(cc)))),
     )
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let start_result = eframe::WebRunner::new()
+            .start(
+                "the_canvas_id",
+                web_options,
+                Box::new(|cc| Ok(Box::new(Befunge93App::new(cc)))),
+            )
+            .await;
+        let loading_text = web_sys::window()
+            .and_then(|w| w.document())
+            .and_then(|d| d.get_element_by_id("loading_text"));
+
+        if let Some(loading_text) = loading_text {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p>The app has crashed. See the developer console for details.</p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
 }
 
 #[derive(Default)]
